d9cdcd84dbd5effc808071673786cb00f4265adb
Lourdes Mendoza <lourd.mendoza@gmail.com>
2021-04-02 21:03:04 -0600

    Write new DOM elements

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 // Create variables targetting the relevant DOM elements here 👇
+var title = document.querySelector('.cover-title');
+var tagline1 = document.querySelector('.tagline-1');
+var tagline2 = document.querySelector('.tagline-2');
+var cover = document.querySelector('.cover-image');
 
 
 // We've provided a few variables below
