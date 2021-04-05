3e92a573b5a63dc136e439cbfff5ff974c00b531
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-04-04 19:55:27 -0600

    Include README.md from the project

--- src/main.js
+++ src/main.js
@@ -49,15 +49,19 @@ function createRandomCover() {
   showRandomPhoto();
   currentCover = new Cover(cover, title, tagline1, tagline2);
 }
+
 function showRandomTitle() {
   title.innerText = titles[getRandomIndex(titles)];
 }
+
 function showRandomTagline1() {
   tagline1.innerText = descriptors[getRandomIndex(descriptors)];
 }
+
 function showRandomTagline2() {
   tagline2.innerText = descriptors[getRandomIndex(descriptors)];
 }
+
 function showRandomPhoto() {
   cover.src = covers[getRandomIndex(covers)];
 }
